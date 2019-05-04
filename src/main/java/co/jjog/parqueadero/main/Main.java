046d200c6e5578da70c9adabde125dcea43c0d8a
Jhon <jhonjames0510@gmail.com>
2019-05-04 17:07:30 -0500

    last commit before start interface

--- src/main/java/co/jjog/parqueadero/main/Main.java
+++ src/main/java/co/jjog/parqueadero/main/Main.java
@@ -1,11 +1,19 @@
 package co.jjog.parqueadero.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/src/main/resources/view/"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 
